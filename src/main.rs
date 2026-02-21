bb289e911a0ed3c973f00e711b1201e0a9c9e34f
Disciplined-22 <129745308+Disciplined-22@users.noreply.github.com>
2026-02-21 13:35:09 +0530

    added the code,  MPSC throughput benchmark demo
    Add educational demo benchmarking std::sync::mpsc throughput with 3 producers and 1 consumer

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::{Instant};
+
+fn main() {
+    let (tx, rx) = mpsc::channel::<String>();
+    // Creates a multi-producer, single-consumer channel.
+    // tx = Sender<u64>
+    // rx = Receiver<u64>
+
+    let producers = 3;
+
+    // Spawn Producers
+    for id in 0..producers {
+        let tx_clone = tx.clone();
+        // Clone sender so multiple threads can send to same channel
+
+        thread::spawn(move || {
+
+            // 1 million msgs send
+            for _ in 0..1_000_000 {
+                tx_clone.send("hello__bro the greatest of all time".to_string()).unwrap();
+            }
+        });
+
+        println!("Producer {} started", id);
+    }
+
+    drop(tx); 
+    // Drops the original sender.
+    // Important because channel closes ONLY when all senders are dropped.
+    // But producers still hold cloned senders.
+
+    // Consumer
+    thread::spawn(move || {
+        let mut count: u64 = 0;
+        let start = Instant::now(); // Note intila timing 
+    
+        while let Ok(_v) = rx.recv() { // wait for it to get completed 
+            
+            count += 1;
+        }
+    
+
+        let elapsed = start.elapsed().as_secs_f64(); // calulated the elapsed time 
+        let throughput = count as f64 / elapsed; // and final throughput
+    
+        println!("Total received: {}", count);
+        println!("Elapsed time: {:.3} seconds", elapsed);
+        println!("Throughput: {:.0} msgs/sec", throughput);
+    })
+    .join()
+    .unwrap();
+}
